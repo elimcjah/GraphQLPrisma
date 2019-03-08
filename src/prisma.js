beee4aa5c65c85db5b0ab7e8c40a9039a4fa65b4
Elijah McClendon <elijah@fungry.com>
2019-03-08 14:36:39 -0700

    CreatePostForUser creates a Post with a connection to User

--- src/prisma.js
+++ src/prisma.js
@@ -12,25 +12,70 @@ const prisma = new Prisma({
  * prisma.exists
  * */
 
+const createPostForUser = async (authorId, data) => {
+    try {
+        const post = await prisma.mutation.createPost({
+            data: {
+                ...data,
+                author: {
+                    connect: {
+                        id: authorId
+                    }
+                }
+            }
+        }, `{ id }`);
+
+        const user = await prisma.query.user({
+            where: {
+                id: authorId
+            }
+        }, '{ id name email posts { id title body published }}');
+
+        return user;
+    } catch (e) {
+                throw new Error(`createPostForUser \n ${e}`);
+    }
+};
+
+// createPostForUser('cjszcduzk01v60852wh7adro2', {
+//     title: '231',
+//     body: '231 body',
+//     published: true
+// }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
+
+
+
 //
 // prisma.query.comments(null, `{id text author { id name } }`).then((data) => {
 //     console.log(JSON.stringify(data, undefined, 2));
 // });
 
-prisma.mutation.createPost({
-    data: {
-        title: '137 Friday Title',
-        body: '137 Friday Body',
-        published: true,
-        author: {
-            connect: {
-                id: 'cjsz6hlw500ux08521sul7u2j'
-            }
-        }
-    }
-}, `{ id title body published}`).then((data) => {
-    console.log(JSON.stringify(data, undefined, 2));
-    prisma.query.users(null, `{ id name email posts { id title }}`).then((data) => {
-        console.log(JSON.stringify(data, undefined, 2));
-    });
-})
+// prisma.mutation.createPost({
+//     data: {
+//         title: '144 Friday Title',
+//         body: '144 Friday Body',
+//         published: false,
+//         author: {
+//             connect: {
+//                 id: 'cjsz6hlw500ux08521sul7u2j'
+//             }
+//         }
+//     }
+// }, `{ id title body published}`).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//     prisma.query.users(null, `{ id name email posts { id title }}`).then((data) => {
+//         console.log(JSON.stringify(data, undefined, 2));
+//     });
+// })
+// prisma.mutation.updatePost({
+//     data: {
+//         published: true
+//     },
+//     where: {
+//         id: "cjt0j3lkn003u0834jexr4n6c"
+//     }
+// }, `{ id title body published }`).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+// });
